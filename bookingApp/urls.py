de8e612495f681913fa5dbf4e3f9f2b5e4702b8c
Bilal Salim <bilall1999775@outlook.com>
2023-11-07 20:16:57 +0300

    booking

--- bookingApp/urls.py
+++ bookingApp/urls.py
@@ -6,5 +6,7 @@ from .import views
 
 urlpatterns = [
     path("post_student/",views.post_student,name="post_student"),
-    path("get_courses/",views.get_courses,name="get_courses")
+    path("get_courses/",views.get_courses,name="get_courses"),
+    path("get_details/<int:id>/",views.get_details,name="get_details"),
+    path("deleteItem/<int:id>/",views.deleteItem,name="deleteItem")
 ]
--- bookingApp/views.py
+++ bookingApp/views.py
@@ -1,36 +1,62 @@
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .models import Booking,Course
+from django.core import serializers
+
 import json
 
 @csrf_exempt
 def post_student(request):
     if request.method == 'POST':
-        data=json.loads(request.body)
-        booking=Course(
-            coursesName= data['firstName'],
-            is_puplish=1,
-            cateoryID_id=1
-             # lastnNme=data['lastName'],
-            # email=data['email'],
-            # phone=data['phone'],
-            # age=data['age'],
-            # coursesID=['coursesID']
-        )
-        booking.save()
+        if request.body:
+            try:
+                data = json.loads(request.body)
+                # Now, you can access and use the JSON data as needed
+                firstName = data.get('firstName', '')
+                lastnNme = data.get('lastnNme', '')
+                email = data.get('email', '')
+                phone = data.get('phone', '')
+                age = data.get('age', '')
+                
+                # Create and save the Course instance here
 
-        response_data = {
-            'firstName': data.get('firstName', ''),
-            'lastName': data.get('lastName', ''),
-        }
-        return JsonResponse({'data': response_data}, status=200)
+                response_data = {
+                     'firstName': data.get('firstName', ''),
+                     'lastName': data.get('lastName', ''),
+                    ' email': data.get('email', ''),
+                    'email': email,
+                    'phone': phone,
+                     'age': age,
+
+                }
+
+                return JsonResponse({'data': response_data}, status=200)
+            except json.JSONDecodeError:
+                return JsonResponse({'error': 'Invalid JSON format'}, status=400)
+        else:
+            return JsonResponse({'error': 'Empty request body'}, status=400)
     else:
         return JsonResponse({'error': 'Invalid method'}, status=405)
     
-
 def get_courses(request):
     courses=Course.objects.all()
     data = [{'id': course.id, 'name': course.coursesName,'Description': course.Description,'img':course.image,'is_puplish':course.is_puplish,'cateoryID':course.cateoryID.categoryName } for course in courses]
     return JsonResponse(data,safe=False)
 
+    
+def get_details(request,id):
+    try:
+        course = Course.objects.get(pk=id)
+        course_data = serializers.serialize('json', [course])
+        return JsonResponse({'course':course_data}, safe=False)
+    except Course.DoesNotExist:
+        return JsonResponse({'error': 'Course not found'})
+    
+        
+def deleteItem(request,id):
+    try:
+        course = Course.objects.get(pk=id)
+        course.delete()  # This will delete the course
+        return JsonResponse({'message': 'Deleted item successfully'})
+    except Course.DoesNotExist:
+        return JsonResponse({'error': 'Course not found'})
